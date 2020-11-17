850b739506614f00f85ce0116d248c0f8f35b168
yama24 <maulana24@live.com>
2020-11-17 17:15:23 +0700

    Update banner.php

--- application/views/templates/banner.php
+++ application/views/templates/banner.php
@@ -1,13 +1,15 @@
 <?php $banner = $this->Settings_model->getBanner(); ?>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-	<ol class="carousel-indicators">
-		<?php
-		foreach ($banner->result_array() as $key => $value) {
-			$active = ($key == 0) ? 'active' : '';
-			echo '<li data-target="#carouselExampleIndicators" data-slide-to="' . $key . '" class="' . $active . '"></li>';
-		}
-		?>
-	</ol>
+	<?php if (count($banner->result_array()) > 1) { ?>
+		<ol class="carousel-indicators">
+			<?php
+			foreach ($banner->result_array() as $key => $value) {
+				$active = ($key == 0) ? 'active' : '';
+				echo '<li data-target="#carouselExampleIndicators" data-slide-to="' . $key . '" class="' . $active . '"></li>';
+			}
+			?>
+		</ol>
+	<?php } ?>
 	<div class="carousel-inner">
 		<?php
 		foreach ($banner->result_array() as $key => $value) {
@@ -18,12 +20,14 @@
 		}
 		?>
 	</div>
-	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
+	<?php if (count($banner->result_array()) > 1) { ?>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	<?php } ?>
 </div>
